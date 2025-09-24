cb85e83e43a63e306cf0f5a52ccdf4dcf2822605
marianarestrepo26 <marianartpo08@gmail.com>
2025-09-23 20:35:40 -0500

    Feature: Create main menu

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using sprint_2.Models;
 var db = new UserController(
     "Server=168.119.183.3;Database=tren_hubble;User=root;Password=g0tIFJEQsKHm5$34Pxu1;Port=3307");
 
-foreach (var user in db.GetAllUsers())
+/*foreach (var user in db.GetAllUsers())
 {
     Console.WriteLine($" {user.Id} {user.FirstName} {user.Email}");
 }
@@ -12,7 +12,7 @@ foreach (var user in db.GetAllUsers())
 foreach (var user in db.GetAllUsers())
 {
     Console.WriteLine($" {user.Id} {user.FirstName} {user.Email}");
-}
+}*/
 
 bool flag = true;
 
@@ -22,7 +22,7 @@ while (flag)
                   "1. Crear usuario \n" +
                   "2. Actualizar usuario \n" +
                   "3. Eliminar usuario \n" +
-                  "4. Buscar usuario\n" +
+                  "4. Consultas\n" +
                   "5. Salir\n" +
                   ">> ");
     string option = Console.ReadLine();
@@ -42,11 +42,125 @@ while (flag)
             break;
         
         case "4":
+            bool main = true;
+            while (main)
+            {
+                Console.Write("---- Consultas del sistema ----\n" +
+                                  "1. Listar usuarios\n" +
+                                  "2. Contar usuarios\n" +
+                                  "3. Mostrar usuarios\n" +
+                                  "4. Volver al menu principal\n" +
+                                  ">> ");
+                string opt = Console.ReadLine();
+                switch (opt)
+                {
+                    case "1":
+                        bool menuList = true;
+                        while (menuList)
+                        {
+                            Console.Write("---- Listar usuarios ----\n\n" +
+                                          "1. Listar nombres y correos\n" +
+                                          "2. Listar todos los usuarios\n" +
+                                          "3. Listar usuarios por ciudad\n" +
+                                          "4. Listar usuarios por edad mínima\n" +
+                                          "5. Listar usuarios por género\n" +
+                                          "6. Listar usuarios por país\n" +
+                                          "7. Últimos usuarios registrados\n" +
+                                          "8. Usuarios ordenados por apellido\n" +
+                                          "9. Volver al menú anterior\n" +
+                                          ">> ");
+                            string optList = Console.ReadLine();
+                            switch (optList)
+                            {
+                                case "1":
+                                    break;
+                                case "2":
+                                    break;
+                                case "3":
+                                    break;
+                                case "4":
+                                    break;
+                                case "5":
+                                    break;
+                                case "6":
+                                    break;
+                                case "7":
+                                    break;
+                                case "8":
+                                    break;
+                                case "9":
+                                    menuList = false;
+                                    break;
+                            }
+                        }
+                        
+                        break;
+                    
+                    case "2":
+                        bool mainCount = true;
+                        while (mainCount)
+                        {
+                            Console.Write("---- Contar usuarios ----\n\n" +
+                                          "1. Contar usuarios por ciudad\n " +
+                                          "2. Contar usuarios por país\n" +
+                                          "3. Contar usuarios totales" +
+                                          "4. Volver al menú anterior\n" +
+                                          ">> ");
+                            string optCount = Console.ReadLine();
+                            switch (optCount)
+                            {
+                                case "1":
+                                    break;
+                                case "2":
+                                    break;
+                                case "3":
+                                    break;
+                                case "4":
+                                    mainCount = false;
+                                    break;
+                            }
+                        }
+                        break;
+                    
+                    case "3":
+                        bool menuShow = true;
+                        while (menuShow)
+                        {
+                            Console.Write("---- Mostrar usuarios ----\n\n" +
+                                          "1. Usuarios sin dirección\n" +
+                                          "2. Usuarios sin teléfono\n" +
+                                          "3. Ver usuario por correo\n" +
+                                          "4. Ver usuario por Id" +
+                                          "5. Volver al menú anterior\n" +
+                                          ">> ");
+                            string optShow = Console.ReadLine();
+                            switch (optShow)
+                            {
+                                case "1":
+                                    break;
+                                case "2":
+                                    break;
+                                case "3":
+                                    break;
+                                case "4":
+                                    break;
+                                case "5":
+                                    menuShow = false;
+                                    break;
+                            }
+                        }
+                        break;
+                    
+                    case "4":
+                        main = false;
+                        break;
+                }
+            }
 
             break;
         
         case "5":
-
+            flag = false;
             break;
         
         default:
